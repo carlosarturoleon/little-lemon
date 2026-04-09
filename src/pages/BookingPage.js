74b5fd71d5a1d7a1aab5471c0516b8de3cc4be09
carlosarturoleon <ctr486@yahoo.com>
2026-04-08 23:51:50 -0500

    Add BookingForm controlled component with state for all form fields

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,8 +1,10 @@
+import BookingForm from '../components/BookingForm';
+
 function BookingPage() {
   return (
     <section className="booking-page container">
       <h1>Reserve a Table</h1>
-      <p>Booking form coming soon.</p>
+      <BookingForm />
     </section>
   );
 }
