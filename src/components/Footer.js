d184eca96e348739b7b4c7868be3b934bb369f7e
carlosarturoleon <ctr486@yahoo.com>
2026-04-08 23:36:31 -0500

    Set up semantic HTML structure with Header, Nav, Main and Footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+function Footer() {
+  return (
+    <footer>
+      <img src="/Applying design fundamentals in Figma - Assets/Logo.svg" alt="Little Lemon Logo" />
+      <nav>
+        <p>Doormat Navigation</p>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/reservations">Reservations</a></li>
+          <li><a href="/order">Order Online</a></li>
+          <li><a href="/login">Login</a></li>
+        </ul>
+      </nav>
+      <address>
+        <p>Contact</p>
+        <p>Address</p>
+        <p>Phone number</p>
+        <p>Email</p>
+      </address>
+      <section>
+        <p>Social Media Links</p>
+        <p>Address</p>
+        <p>Phone number</p>
+        <p>Email</p>
+      </section>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from './Nav';
+
+function Header() {
+  return (
+    <header>
+      <img src="/Applying design fundamentals in Figma - Assets/Logo.svg" alt="Little Lemon Logo" />
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
